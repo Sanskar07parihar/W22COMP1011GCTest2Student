9fab46d3d58686aae39899f934d82a03ad628988
Sanskar Parihar <200489011@student.georgianc.on.ca>
2022-07-24 22:18:02 -0400

    Update

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module ca.georgiancollege.comp1011m2022ice5 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    //Warning: Don't delete this part by accident
-    opens ca.georgiancollege.comp1011m2022ice5 to javafx.fxml;
-    exports ca.georgiancollege.comp1011m2022ice5;
-}
